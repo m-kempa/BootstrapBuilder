8e02efe0e2edfbdd4dbdec7d535a4f6eff3fc38a
JoannaBrnk <43933949+JoannaBrnk@users.noreply.github.com>
2020-12-21 00:07:21 +0100

    extend by footer and body section

--- src/main/java/pl/put/poznan/builder/logic/BootstrapBuilder.java
+++ src/main/java/pl/put/poznan/builder/logic/BootstrapBuilder.java
@@ -49,12 +49,7 @@ public class BootstrapBuilder {
             //brand name
             result += "<a class=\"navbar-brand\" href=\"#\">" + this.bootstrapData.header.name + "</a>";
 
-            //collapsing button
-            if (this.bootstrapData.header.collapsable) {
-                result += "<button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#collapsibleNavbar\">"
-                        + "<span class=\"navbar-toggler-icon\"></span></button>\n";
-                result += "<div class=\"collapse navbar-collapse\" id=\"collapsibleNavbar\">";
-            }
+
 
 
 
@@ -70,6 +65,45 @@ public class BootstrapBuilder {
             result += "</nav>";
         }
 
+        //body (container)
+        result += "<main class=\"";
+        if (this.bootstrapData.body.container_fluid) {
+            result += "container-fluid";
+        } else {
+            result += "container";
+        }
+        result += "\" style=\"flex: 1 0 auto";
+        if (this.bootstrapData.header.fixed) {
+            result += "; margin-top: 70px";
+        } else {
+            result += "; margin-top: 10px";
+        }
+        result += "\">";
+        if (this.bootstrapData.body.content.equals("Lorem ipsum")) {
+            for (int i = 0; i < 8; i++)
+                result += "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet, elit magna vulputate arcu, vel tempus metus leo non est. Etiam sit amet lectus quis est congue mollis. Phasellus congue lacus eget neque. Phasellus ornare, ante vitae consectetuer consequat, purus sapien ultricies dolor, et mollis pede metus eget nisi. Praesent sodales velit quis augue. Cras suscipit, urna at aliquam rhoncus, urna quam viverra nisi, in interdum massa nibh nec erat.</p>";
+        } else {
+            result += this.bootstrapData.body.content;
+        }
+        result += "</main>";
+
+
+        //footer
+        if (this.bootstrapData.isFooter) {
+            result += "<footer class=\"";
+            switch (this.bootstrapData.header.style) {
+                case "dark": result += "bg-dark navbar-dark"; break;
+                case "light": result += "bg-light navbar-light"; break;
+                case "blue": result += "bg-primary navbar-dark"; break;
+                default: result += "bg-success navbar-dark";
+            }
+            result += "\" style=\"text-align: center; flex-shrink: 0\">";
+            result += "<p>" + this.bootstrapData.footer.content + "</p>";
+            result += "<a href=\"mailto:" + this.bootstrapData.footer.contact + "\" target=\"_top\">";
+            result += this.bootstrapData.footer.contact;
+            result += "</a></footer>";
+        }
+
 
         System.out.println(result);
         return result;
@@ -83,3 +117,7 @@ public class BootstrapBuilder {
     public String getTitle(){ return this.bootstrapData.title; }
 
 }
+
+
+
+
