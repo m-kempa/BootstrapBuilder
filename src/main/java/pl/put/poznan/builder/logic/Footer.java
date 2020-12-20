49c0cbc5ea583603927019545355fd3a4354f5da
JoannaBrnk <43933949+JoannaBrnk@users.noreply.github.com>
2020-12-21 00:05:10 +0100

    Footer class information

---
+++ src/main/java/pl/put/poznan/builder/logic/Footer.java
@@ -0,0 +1,14 @@
+package pl.put.poznan.builder.logic;
+
+/**
+ * Class that contains information about the footer in the bootstrap based webpage
+ *
+ * @author Joanna Bronka
+ * @version 1.0
+ */
+public class Footer {
+    public String content;
+    public String contact;
+    public String style;
+
+}
