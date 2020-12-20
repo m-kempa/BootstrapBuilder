3de63d734a49c70b723b14eff6e895305cdcd104
kkuubb <49914218+kkuubb@users.noreply.github.com>
2020-12-20 20:24:15 +0100

    information about the header

---
+++ src/main/java/pl/put/poznan/builder/logic/Header.java
@@ -0,0 +1,18 @@
+package pl.put.poznan.builder.logic;
+import java.util.List;
+
+/**
+ * Class that contains information about the header in the bootstrap based webpage
+ *
+ * @author Jakub Różycki
+ * @version 1.0
+ */
+public class Header {
+    public String name;
+    public Boolean searchform;
+    public String style;
+    public Boolean fixed;
+    public Boolean collapsable;
+
+
+}
