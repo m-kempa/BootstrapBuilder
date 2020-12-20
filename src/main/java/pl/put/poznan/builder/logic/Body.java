a4eab65e2f4313b4948d529faa9598aa0704eaf9
JoannaBrnk <43933949+JoannaBrnk@users.noreply.github.com>
2020-12-21 00:03:09 +0100

    Body class information

---
+++ src/main/java/pl/put/poznan/builder/logic/Body.java
@@ -0,0 +1,12 @@
+package pl.put.poznan.builder.logic;
+
+/**
+ * Class that contains information about the body in the bootstrap based webpage from the json
+ *
+ * @author Joanna Bronka
+ * @version 1.0
+ */
+public class Body{
+    public Boolean container_fluid;
+    public String content;
+}
