fad0d70369bdda90ef4929fe5de460b70ce59e8e
Asia <joanna.brnk@gmail.com>
2021-01-23 21:12:10 +0100

    add tags

--- src/main/java/pl/put/poznan/builder/logic/BootstrapBuilder.java
+++ src/main/java/pl/put/poznan/builder/logic/BootstrapBuilder.java
@@ -48,7 +48,7 @@ public class BootstrapBuilder {
             result += element.createNavbar(this.bootstrapData);
 
             //brand name
-            result += "<a class=\"navbar-brand\" href=\"#\">" + this.bootstrapData.header.name + "</a>";
+            result += "<a class=\"navbar-brand tag=brand_name\" href=\"#\">" + this.bootstrapData.header.name + "</a>";
 
             //collapsing button
             if (this.bootstrapData.header.collapsable) {
@@ -58,7 +58,7 @@ public class BootstrapBuilder {
             }
 
             //navbar subpages
-            result += "<ul class=\"navbar-nav mr-auto\">";
+            result += "<ul class=\"navbar-nav tag=subpages mr-auto\">";
             if (this.bootstrapData.header.subpages.size() > 0) {
                 for (Header.Subpage sub : this.bootstrapData.header.subpages) {
                     result += "<li class=\"nav-item";
@@ -79,7 +79,7 @@ public class BootstrapBuilder {
 
             //clock
             if (this.bootstrapData.header.clock) {
-                result += "<li class=\"nav-item\" >"
+                result += "<li class=\"nav-item\" tag=clock>"
                         + "<div class=\"card bg-secondary text-white\">"
                         + "<h3 class=\"card-title text-center\">"
                         + "<div class=\"d-flex flex-wrap justify-content-center mt-2\">"
@@ -96,7 +96,7 @@ public class BootstrapBuilder {
 
             //search form
             if (this.bootstrapData.header.searchform) {
-                result += "<form class=\"form-inline\" action=\"#\">"
+                result += "<form class=\"form-inline\" tag=search action=\"#\">"
                         + "<input class=\"form-control mr-sm-2\" type=\"text\" placeholder=\"Search\">"
                         + "<button class=\"btn btn-secondary\" type=\"submit\">Search</button></form>";
             }
@@ -110,7 +110,7 @@ public class BootstrapBuilder {
         }
 
         //body (container)
-        result += "<main class=\"";
+        result += "<main tag=body class=\"";
         if (this.bootstrapData.body.container_fluid) {
             result += "container-fluid";
         } else {
