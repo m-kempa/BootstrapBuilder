fd687f21f4dd03cba2d79085e66a697da0b729d9
Asia <joanna.brnk@gmail.com>
2021-01-23 21:42:19 +0100

    refactor test

--- src/main/java/tests/pl/put/poznan/builder/BootstrapBuilderTest.java
+++ src/main/java/tests/pl/put/poznan/builder/BootstrapBuilderTest.java
@@ -3,26 +3,34 @@ package tests.pl.put.poznan.builder;
 import org.junit.jupiter.api.Test;
 import pl.put.poznan.builder.logic.BootstrapBuilder;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.Scanner;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class BootstrapBuilderTest {
     @Test
-    void create_clock() throws FileNotFoundException {
+    void create_clock() {
 
-        File file = new File("src/examples/with_clock.json");
-        String content = new Scanner(file).useDelimiter("\\Z").next();
+        BootstrapBuilder bootstrapBuilder = getBootstrapBuilder("src/examples/with_clock.json");
+
+        assertThat(bootstrapBuilder.getBodyContent(), containsString("tag=clock"));
+    }
+
+    private BootstrapBuilder getBootstrapBuilder(String pathname) {
+        File file = new File(pathname);
+        String content = null;
+        try {
+            content = new Scanner(file).useDelimiter("\\Z").next();
+        } catch (FileNotFoundException e) {
+            fail(e.getMessage());
+        }
 
         BootstrapBuilder bootstrapBuilder = new BootstrapBuilder();
         bootstrapBuilder.setJsonContent(content.toString());
-
-        assertThat(bootstrapBuilder.getBodyContent(), containsString("tag=clock"));
+        return bootstrapBuilder;
     }
 }
