cb84c9894423b7a5bb3f8d05f3c5d84318dca60f
Mateusz Kempa <kkuubb92@gmai.com>
2021-01-24 16:52:33 +0100

    mock improved

--- src/test/java/pl/put/poznan/builder/logic/mocktest.java
+++ src/test/java/pl/put/poznan/builder/logic/mocktest.java
@@ -11,9 +11,9 @@ class BuilderTest {
         void testCreateNavbar() {
                 BootstrapData DataMocked = mock(BootstrapData.class);
                 String result = "";
-                when(DataMocked.getResponsive()).thenReturn(true);
                 when(DataMocked.getHeaderFixed()).thenReturn(true);
                 when(DataMocked.getHeaderStyle()).thenReturn("light");
+                when(DataMocked.getResponsive()).thenReturn(true);
                 assertEquals("<nav class=\"navbar navbar-expand-md bg-light navbar-light fixed-top\">", element.createNavbar(DataMocked, result));
         }
 
