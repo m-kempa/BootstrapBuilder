51455151c6a15fc2521b9c86e7ddf8ebd2018ab9
Asia <joanna.brnk@gmail.com>
2021-01-24 11:58:41 +0100

    add header style tests

--- src/main/java/tests/pl/put/poznan/builder/BootstrapBuilderHeaderTest.java
+++ src/main/java/tests/pl/put/poznan/builder/BootstrapBuilderHeaderTest.java
@@ -7,7 +7,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-import static com.sun.tools.doclint.Entity.not;
+//import static com.sun.tools.doclint.Entity.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.not;
@@ -25,6 +25,30 @@ class BootstrapBuilderHeaderTest {
     void do_not_create_header() {
         BootstrapBuilder bootstrapBuilder = getBootstrapBuilder("src/examples/without_any_components.json");
         assertThat(bootstrapBuilder.getBodyContent(), not(containsString("tag=navbar")));
+    }
+
+    @Test
+    void header_style_dark(){
+        BootstrapBuilder bootstrapBuilder = getBootstrapBuilder("src/examples/dark_header.json");
+        assertThat(bootstrapBuilder.getBodyContent(), containsString("bg-dark navbar-dark"));
+    }
+
+    @Test
+    void header_style_light(){
+        BootstrapBuilder bootstrapBuilder = getBootstrapBuilder("src/examples/light_header.json");
+        assertThat(bootstrapBuilder.getBodyContent(), containsString("bg-light navbar-light"));
+    }
+
+    @Test
+    void header_style_blue(){
+        BootstrapBuilder bootstrapBuilder = getBootstrapBuilder("src/examples/blue_header.json");
+        assertThat(bootstrapBuilder.getBodyContent(), containsString("bg-primary navbar-dark"));
+    }
+
+    @Test
+    void header_style_default(){
+        BootstrapBuilder bootstrapBuilder = getBootstrapBuilder("src/examples/default_header.json");
+        assertThat(bootstrapBuilder.getBodyContent(), containsString("bg-success navbar-dark"));
     }
 
     @Test
@@ -46,9 +70,21 @@ class BootstrapBuilderHeaderTest {
     }
 
     @Test
-    void do_not_collapsing_button() {
+    void do_not_create_collapsing_button() {
         BootstrapBuilder bootstrapBuilder = getBootstrapBuilder("src/examples/without_any_components.json");
         assertThat(bootstrapBuilder.getBodyContent(), not(containsString("tag=collapsing_button")));
+    }
+
+    @Test
+    void create_search_form() {
+        BootstrapBuilder bootstrapBuilder = getBootstrapBuilder("src/examples/with_all_components.json");
+        assertThat(bootstrapBuilder.getBodyContent(), containsString("tag=search"));
+    }
+
+    @Test
+    void do_not_create_search_form() {
+        BootstrapBuilder bootstrapBuilder = getBootstrapBuilder("src/examples/without_any_components.json");
+        assertThat(bootstrapBuilder.getBodyContent(), not(containsString("tag=search")));
     }
 
     @Test
